13b33e2e9354ee2f50b3e7209ef3ddd153a04606
Adina Shanholtz <adinas@microsoft.com>
2016-02-02 21:49:18 -0500

    init commit

---
+++ CollectThemAll/Assets/Scripts/GameController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+    public Text countText;
+    public Text winText;
+
+    private int count;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AddOne()
+    {
+        count++;
+        SetCountText();
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
---
+++ CollectThemAll/Assets/Scripts/ItemRotator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemRotator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+	}
+}
