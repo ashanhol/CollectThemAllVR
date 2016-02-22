3d587ab1ca0d5c40b07a79fd68a719797abd7e49
Adina Shanholtz <adinas@microsoft.com>
2016-02-22 16:04:54 -0500

    Added Fireworks
    Added Fireworks when player wins.

--- CardboardDemo/Assets/Our Assets/Scripts/GameController.cs
+++ CardboardDemo/Assets/Our Assets/Scripts/GameController.cs
@@ -8,16 +8,23 @@ public class GameController : MonoBehaviour
     //Global Variables
     public Text countText;
     public Text winText;
+    public ParticleSystem fireworks;
+
 
     private int count;
 
 
-    // Use this for initialization
+    //Initialization of variables
+    //Grabs firework particle system and tells it to not emmit. 
     void Start()
     {
         count = 0;
         SetCountText();
         winText.text = "";
+        GameObject fireworkObject = GameObject.FindWithTag("Fireworks");
+        fireworks = fireworkObject.GetComponent<ParticleSystem>();
+        var em = fireworks.emission;
+        em.enabled = false;
     }
 
     // Update is called once per frame
@@ -35,13 +42,18 @@ public class GameController : MonoBehaviour
 
     //SetCountText() updates the count to the screen and displays 
     //"You Win" to the screen when collectables all collected
+    //Grabs firework particle system and tells it to start emmiting when goal is reached. 
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
         if (count >= 12)
         {
             winText.text = "You Win!";
-          
+            GameObject fireworkObject = GameObject.FindWithTag("Fireworks");
+            fireworks = fireworkObject.GetComponent<ParticleSystem>();
+            var em = fireworks.emission;
+            em.enabled = true;
+
         }
     }
 }
